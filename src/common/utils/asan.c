2f56ff4561487a143a23f7c3182431a35f81d275
boburning <boburning@gmail.com>
2026-08-15 19:45:04 -0400

    style: format sanitizer options

--- src/common/utils/asan.c
+++ src/common/utils/asan.c
@@ -1,9 +1,10 @@
 
 __attribute__((externally_visible))
-const char *__asan_default_options() {
+const char *__asan_default_options()
+{
 #ifdef PLATFORM_MIYOOMINI
-  return "log_path=/mnt/SDCARD/.tmp_update/logs/ASAN.log:halt_on_error=0";
+    return "log_path=/mnt/SDCARD/.tmp_update/logs/ASAN.log:halt_on_error=0";
 #else
-  return "halt_on_error=1";
+    return "halt_on_error=1";
 #endif
 }
